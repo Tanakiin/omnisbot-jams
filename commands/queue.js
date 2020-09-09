c18a77881270c9093ef9bd95fddf8721d2493780
Tanakiin <64032384+Tanakiin@users.noreply.github.com>
2020-09-09 20:40:05 +0300

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -1,11 +1,11 @@
-const { DiscordAPIError } = require("discord.js");
-
 exports.run = (client, message, args) => {
   const serverQueue = message.client.queue.get(message.guild.id);
   if (!serverQueue) return message.channel.send("There is nothing playing.");
-    const queue1 = new Discord.MessageEmbed()
-      .setTitle('Current Song and Queue')
-      .addField(`${serverQueue.songs.map((song) => `**-** ${song.title}`).join("\n")}`)
-      .addField('__**Now Playing**__', `${serverQueue.songs[0].title}`)
-msg.channel.send(queue1)
-}
+  return message.channel.send(`
+__**Song queue:**__
+
+${serverQueue.songs.map((song) => `**-** ${song.title}`).join("\n")}
+
+**Now playing:** ${serverQueue.songs[0].title}
+		`);
+};
